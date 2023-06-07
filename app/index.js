e660c6e39428d8ee8d5f52b0ecbf57b9b2414c65
Tomas Ferreras <hellotomasdev@gmail.com>
2023-06-06 21:23:40 -0300

    add basic folder structure and assets

---
+++ app/index.js
@@ -0,0 +1,44 @@
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { COLORS, ICONS, IMAGES, SIZES, icons, images } from '../constants';
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+  WelcomeScreen,
+} from '../components';
+
+const Home = () => {
+  const router = useRouter();
+  console.log(icons.menu);
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerShadowVisible: true,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimensions="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimensions="100%" />
+          ),
+          headerTitle: '',
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
